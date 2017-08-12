11fcfd8b0dd73d14fff1da644ed75a1f3c4242a9
yasuoyuhao <lf2sam111@gmail.com>
2017-08-12 10:56:20 +0800

    1.3 Upgrade 2017/08/10

--- Extension.swift
+++ Extension.swift
@@ -473,7 +473,25 @@ extension UIImage {
         
         return UIImage(cgImage: cgImage)
     }
+    
+    func scale(newWidth: CGFloat) -> UIImage {
 
+        if self.size.width == newWidth {
+            return self
+        }
+        
+        let scaleFactor = newWidth / self.size.width
+        let newHeight = self.size.height * scaleFactor
+        let newSize = CGSize(width: newWidth, height: newHeight)
+        
+        UIGraphicsBeginImageContextWithOptions(newSize, false, 0.0);
+        self.draw(in: CGRect(x: 0, y: 0, width: newWidth, height: newHeight))
+        
+        let newImage: UIImage? = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        return newImage ?? self
+    }
 }
 
 //MARK: - GradientView
